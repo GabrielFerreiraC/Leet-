053e6521fed377ad5bdbb6e69285f923ec24ac93
Gabriel Ferreira <gfdc2204@gmail.com>
2022-09-16 19:55:40 -0300

    adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Texto a ser traduzido: ");
+Console.WriteLine(
+Console.ReadLine()
+.Replace ("A", "4")
+.Replace ("a", "4")
+.Replace ("E", "3")
+.Replace ("e", "3")
+.Replace ("I", "1")
+);   
+
